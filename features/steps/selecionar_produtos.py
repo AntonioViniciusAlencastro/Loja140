ed595f54bba902c7386985e7501ff013311102d2
AntonioViniciusAlencastro <avalencastros@gmail.com>
2024-07-01 12:56:15 -0300

    script utilizando behave

---
+++ features/steps/selecionar_produtos.py
@@ -0,0 +1,24 @@
+import time
+from behave import given, when, then
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@given(u'que acesso o site Sauce Demo')
+def step_impl(context):
+        context.driver= webdriver.Chrome()
+        context.driver.maximize_window() 
+        #context.driver.implicitly_wait(5)
+        context.driver.get("https://www.saucedemo.com/")
+
+@when(u'preencho os dados de login com usuario{usuario} e senha{senha}')
+def step_impl(context, usuario, senha):
+       context.driver.find_element(By.ID , "user-name").send_keys(usuario)
+       context.driver.find_element(By.ID , "password").send_keys(senha) 
+       context.driver.find_element(By.ID , "login-button").click() 
+    
+@then(u'sou direcionado para pagina Home')
+def step_impl(context):
+        assert  context.driver.find_element (By.CSS_SELECTOR, "span.title").text =="Products"
+       # time.sleep(2)
+        context.driver.quit()
+
